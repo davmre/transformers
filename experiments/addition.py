7602cd35c3ad01dbceb100ef10b2f82876d81950
Dave Moore <davmre@gmail.com>
2022-11-14 23:51:50 -0800

    Add command-like config and adopt the Flax TrainState abstraction.

--- experiments/addition.py
+++ experiments/addition.py
@@ -1,63 +1,90 @@
 from re import X
 
-import tensorboard
+from absl import app
+from absl import flags
+from ml_collections import config_dict
+from ml_collections import config_flags
+import tensorboardX
 
 import jax
 from jax import numpy as jnp
 import numpy as np
 
 from flax import linen as nn
+from flax.training import train_state
 import optax
 
 from daves_transformer_lib import addition_task
 
-num_bits = 31  # TODO: support signed ints for full int32.
-num_iters = 8
-d_head = 8
-num_heads = 12
-d_ff = 256
-dropout_rate = 0.0
-batch_size = 64
-learning_rate = 0.1
-logdir = '/tmp/addition_logs'
-
-key = jax.random.PRNGKey(0)
-
-data_key, weights_key, dropout_key = jax.random.split(key, 3)
-
-model = addition_task.AdditionModel(num_heads=num_heads,
-                                    num_iters=num_iters,
-                                    d_head=d_head,
-                                    d_ff=d_ff,
-                                    dropout=nn.Dropout(rate=dropout_rate,
-                                                       deterministic=True))
-g = addition_task.data_generator(data_key,
-                                 batch_size=batch_size,
-                                 num_bits=num_bits)
-
-init_xs, _ = next(g)
-weights = model.init(key, init_xs[0, ...])
-
-opt = optax.adam(learning_rate)
-opt_state = opt.init(weights)
+# Example invocation:
+# `python3 experiments/addition.py --my_config.num_train_steps=4``
+config = config_dict.ConfigDict()
+config.num_bits = 31  # TODO: support signed ints for full int32.
+config.num_iters = 8
+config.d_head = 8
+config.num_heads = 12
+config.d_ff = 256
+config.dropout_rate = 0.0
+config.batch_size = 64
+config.learning_rate = 1e-4
+config.num_train_steps = 6
+config.logdir = '/tmp/addition_logs'
+_CONFIG = config_flags.DEFINE_config_dict('my_config', config)
 
 
-def loss_fn(weights):
-    xs, y = next(g)
-    xxs = model.apply(weights, xs)
-    return jnp.mean(jnp.sum((y - xxs[..., 0])**2, axis=-1), axis=0)
+def init_train_state(key, model, init_xs, config):
+    weights = model.init(key, init_xs[0, ...])
+    opt = optax.adam(config.learning_rate)
+    return train_state.TrainState(step=0,
+                                  apply_fn=model.apply,
+                                  params=weights,
+                                  tx=opt,
+                                  opt_state=opt.init(weights))
 
 
-@jax.jit
-def train_step(weights, opt_state):
-    loss, grad = jax.value_and_grad(loss_fn)(weights)
-    updates, opt_state = opt.update(grad, opt_state)
-    weights = optax.apply_updates(weights, updates)
-    return loss, weights, opt_state
+def main(_):
+    config = _CONFIG.value
+    key = jax.random.PRNGKey(0)
+
+    data_key, weights_key, dropout_key = jax.random.split(key, 3)
+
+    model = addition_task.AdditionModel(num_heads=config.num_heads,
+                                        num_iters=config.num_iters,
+                                        d_head=config.d_head,
+                                        d_ff=config.d_ff,
+                                        dropout=nn.Dropout(
+                                            rate=config.dropout_rate,
+                                            deterministic=True))
+    g = addition_task.data_generator(data_key,
+                                     batch_size=config.batch_size,
+                                     num_bits=config.num_bits)
+
+    init_xs, _ = next(g)
+    state = init_train_state(weights_key,
+                             model=model,
+                             init_xs=init_xs,
+                             config=config)
+
+    def loss_fn(weights):
+        xs, y = next(g)
+        xxs = model.apply(weights, xs)[..., 0]
+        sq_loss = jnp.mean(jnp.sum((y - xxs)**2, axis=-1), axis=0)
+        return sq_loss, (xs, y, xxs)
+
+    @jax.jit
+    def train_step(state: train_state.TrainState):
+        (loss, aux), grad = jax.value_and_grad(loss_fn,
+                                               has_aux=True)(state.params)
+        state = state.apply_gradients(grads=grad)
+        return loss, aux, state
+
+    writer = tensorboardX.SummaryWriter(logdir=config.logdir)
+    for step in range(config.num_train_steps):
+        loss, aux, state = train_step(state)
+        writer.add_scalar('train/loss', loss, state.step)
+        print(f"step {step} loss {loss}")
+    writer.flush()
 
 
-writer = tensorboard.SummaryWriter(logdir)
-for step in range(100):
-    loss, weights, opt_state = train_step(weights, opt_state)
-    print(f"step {step} loss {loss}")
-    writer.add_scalar('train/loss', loss, step)
+if __name__ == '__main__':
+    app.run(main)
